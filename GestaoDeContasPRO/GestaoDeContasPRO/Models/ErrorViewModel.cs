7dabef1dd3a80396914202080229d1a5cedc9295
miguelrachao <miguel_rachao.96@hotmail.com>
2025-10-04 00:14:50 +0100

    Base project creation.

---
+++ GestaoDeContasPRO/GestaoDeContasPRO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoDeContasPRO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
